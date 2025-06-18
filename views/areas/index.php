d8b1fe8a186b9c51aeef66df794a99c743f5950c
santiago <walterossanti35@gmail.com>
2025-06-18 14:41:00 -0500

    actualizacion de menu

--- views/areas/index.php
+++ views/areas/index.php
@@ -3,15 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Áreas de trabajo</title>
-    <link rel="stylesheet" href="/peoplepro/public/css/nav.css">
-    <link rel="stylesheet" href="/peoplepro/public/css/areas.css">
+    <title>Áreas</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.4/css/responsive.bootstrap5.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.3/css/buttons.dataTables.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.3/css/buttons.bootstrap5.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.bootstrap5.css">
+    <!-- datatable -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css" />
+    <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/peoplepro/public/css/nav.css">
+    <link rel="stylesheet" href="/peoplepro/public/css/usuario.css">
+    <link rel="stylesheet" href="/peoplepro/public/css/datatable.css">
+
 </head>
 <body>
-    <header class="header">
+<header class="header">
         <div class="izquierda">
             <button class="menu-hamburguesa">
                 <span class="linea"></span>
@@ -21,11 +33,22 @@
             <div id="logo"></div> 
         </div>
         <form action="#" class="buscador">  
-        <input type="text" placeholder="Buscar" class="input-icono">
+        <input type="text" placeholder="Buscar..." class="input-buscador">
+        <button type="submit" class="buscador-icono"><i class="bi bi-search"></i></button>
         </form>
         <div class="derecha">
             <p><?= htmlspecialchars($_SESSION["usuario"] ?? 'Usuario') ?></p>
+            <button class="bi-button"><i class="bi bi-gear"></i></button>
         </div>
+        <nav class="nav-seccion">
+            <li>
+                <select id="tema-select">
+                    <option value="claro">Claro</option>
+                    <option value="oscuro">Oscuro</option>
+                </select>
+            </li>
+            <li><a href="#">Cerrar sesión</a></li>
+        </nav>
     </header>
     <nav class="nav-desplegable" id="nav-desplegable">
         <ul class="nav-lista">
