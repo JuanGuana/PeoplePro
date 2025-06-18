c1a7b4bc37d4c42676cbbaf8899d30e7eac2df73
tu_marido_santiago <walterossanti35@gmail.com>
2025-06-18 00:06:58 -0500

    actualizacion de data table 18/06/25

--- public/js/datatable.js
+++ public/js/datatable.js
@@ -1,31 +1,54 @@
 $(document).ready( function () {
     $('#myTable').DataTable({
         responsive: true,
-        info: false,
+        info: false, // ya lo tienes duplicado
+        lengthMenu: [10, 25, 50, 100],
+        pagingType: "simple", // <- SOLO muestra anterior y siguiente
         language: {
-        url: '//cdn.datatables.net/plug-ins/2.3.2/i18n/es-CO.json',
+            url: '//cdn.datatables.net/plug-ins/2.3.2/i18n/es-CO.json',
+            paginate: {
+                previous: '<i class="bi bi-caret-left-fill"></i>',
+                next: '<i class="bi bi-caret-right-fill"></i>'
+            }
         },
-        columnDefs: [
-        { responsivePriority: 1, targets: 0 },
-        { responsivePriority: 2, targets: -1 }
-    ],
-    layout: {
-        topStart: {
-           buttons: [
+        dom: 'Bfrtip',
+        // buttons: ['copy', 'excel', 'pdf', 'print','colvis'],
+        buttons: [
+            {
+                extend: 'copy',
+                text: '<i class="bi bi-clipboard"></i>',
+                className: 'btn btn-secondary ',
+                titleAttr: 'Copiar al portapapeles',
+            },
+            {
+                extend: 'excel',
+                text: '<i class="bi bi-filetype-exe"></i>',
+                className: 'btn btn-success',
+                titleAttr: 'Exportar a Excel',
+            },
+            {
+                extend: 'pdf',
+                text: '<i class="bi bi-filetype-pdf"></i>',
+                className: 'btn btn-danger',
+                titleAttr: 'Exportar a PDF',
+            },
+            {
+                extend: 'print',
+                text: '<i class="bi bi-printer"></i>',
+                className: 'btn btn-info',
+                titleAttr: 'Imprimir',
+            },
             {
                 extend: 'colvis',
-                text: 'Columnas',
-                init: function(api, node, config) {
-                $(node).css({
-                    backgroundColor: 'lightblue',
-                    color: 'white',
-                    borderRadius: '8px',
-                    border: 'none',
-                });
-                }
+                text: '<i class="bi bi-border-width"></i>',
+                className: 'btn btn-warning',
+                titleAttr: 'Seleccionar columnas',
             }
-            ]
-        }
-    },
+        ],
+        columnDefs: [
+            { responsivePriority: 1, targets: 0 },
+            { responsivePriority: 2, targets: -1 }
+        ]
+    });
 });
-});
+
--- views/usuarios/index.php
+++ views/usuarios/index.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.4/css/responsive.bootstrap5.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.3/css/buttons.dataTables.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.2.3/css/buttons.bootstrap5.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.bootstrap5.css">
     <!-- datatable -->
     <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css" />
     <!-- fonts -->
@@ -16,8 +19,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/peoplepro/public/css/nav.css">
     <link rel="stylesheet" href="/peoplepro/public/css/usuario.css">
-    <link rel="stylesheet" href="/peoplepro/public/css/fondo.css">
-    
+    <link rel="stylesheet" href="/peoplepro/public/css/datatable.css">
 
 </head>
 <body>
@@ -31,7 +33,7 @@
             <div id="logo"></div> 
         </div>
         <form action="#" class="buscador">  
-        <input type="text" placeholder="Buscar" class="input-buscador">
+        <input type="text" placeholder="Buscar..." class="input-buscador">
         <button type="submit" class="buscador-icono"><i class="bi bi-search"></i></button>
         </form>
         <div class="derecha">
@@ -124,7 +126,14 @@
     <script src="https://cdn.datatables.net/buttons/3.2.3/js/dataTables.buttons.js"></script>
     <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.dataTables.js"></script>
     <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.colVis.min.js"></script>
-
+    <script src="https://cdn.datatables.net/2.3.2/js/dataTables.bootstrap5.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.bootstrap5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.2.3/js/buttons.print.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
     <!-- Bootstrap JS (uno solo es suficiente) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
 
