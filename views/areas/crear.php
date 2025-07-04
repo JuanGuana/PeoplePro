0bb5321286f0e42ba26a5310bb46bebbfa5ab801
tu_marido_santiago <walterossanti35@gmail.com>
2025-07-04 18:52:21 -0500

    formulario areas con css

--- views/areas/crear.php
+++ views/areas/crear.php
@@ -4,10 +4,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <title>Áreas de trabajo</title>
-    <link rel="stylesheet" href="/peoplepro/public/css/iframesAreas.css">
+    <link rel="stylesheet" href="/peoplepro/public/css/fondo.css">
+    <link rel="stylesheet" href="/peoplepro/public/css/formularios.css">
+    <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <!-- botstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css">
 </head>
 <html>
 <body>
@@ -19,10 +23,9 @@
         <label class="label">Descripción:</label>
         <textarea name="descripcion" rows="3" placeholder="Digita la descripción de area" class="input-descripcion input"></textarea><br>
         
-        </div>  
+        <a href="/peoplepro/public/index.php?action=area" class="btn-volver">Cancelar</a>
         <input type="submit" value="Guardar" class="boton aceptar-btn">
     </form>
-    <a href="/peoplepro/public/index.php?action=area" class="btn-cancelar">Cancelar</a>
 
 </body>
 </html>
