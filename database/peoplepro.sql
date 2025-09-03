05e4e011841b44c795d7f323e79cf1c050d4cfd0
tu_marido_santiago <walterossanti35@gmail.com>
2025-09-02 23:16:03 -0500

    base de datos actualizada 2/09/25

--- database/peoplepro.sql
+++ database/peoplepro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-09-2025 a las 05:05:13
+-- Tiempo de generación: 03-09-2025 a las 06:15:15
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -110,6 +110,13 @@ CREATE TABLE `horarios` (
   `observaciones` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `horarios`
+--
+
+INSERT INTO `horarios` (`id`, `usuario_id`, `fecha`, `fecha_fin`, `hora_inicio`, `hora_fin`, `estado`, `observaciones`) VALUES
+(1, 4, '2025-09-17', '2025-09-11', '01:09:00', '10:11:00', 'Incapacidad', '');
+
 -- --------------------------------------------------------
 
 --
@@ -146,6 +153,7 @@ CREATE TABLE `users` (
   `direccion` text NOT NULL,
   `password` varchar(255) NOT NULL,
   `rol` enum('Admin','Empleado','Seguridad') NOT NULL DEFAULT 'Empleado',
+  `estado` enum('activo','vacaciones','incapacitado','suspendido') DEFAULT 'activo',
   `area_id` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -157,11 +165,12 @@ CREATE TABLE `users` (
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `nombre`, `email`, `telefono`, `direccion`, `password`, `rol`, `area_id`, `created_at`, `updated_at`, `token`, `token_expiracion`) VALUES
-(1, 'laura', 'laura@gmail.com', 0, '', '$2y$10$ZXBSzMa1BC66TPeJow6R7OVvj8hFgOmmL4/jiVSWSCaIw2N.lylxu', 'Admin', 1, '2025-08-12 18:12:46', '2025-09-02 01:45:06', 'fd3b9661fd59182725690bd0bfcc8c57690b7be3aa5c1369f3d1f5ad4058e71e', '2025-09-02 04:45:06'),
-(4, 'daniela', 'daniela@gmail.com', 2147483647, 'ddjhshdushdus', '$2y$10$bwDpMomBmmWgwHxeENUSseTK8v/zpo.oCOnfe5v5mCQ5FuM8M2brW', 'Empleado', 2, '2025-08-26 18:44:00', '2025-08-31 18:02:28', NULL, NULL),
-(5, 'rodrigo', 'rodrigo@gmail.com', 2147483647, 'tu maldito amor', '$2y$10$d6ZI2HMRc0PWzVHhM94Ccemh3LFnClBF0zN68jrOfx1a6Y.mgmm8K', 'Seguridad', 1, '2025-08-27 21:52:08', '2025-08-27 21:52:08', NULL, NULL),
-(6, 'santiago', 'walterossanti35@gmail.com', 2147483647, 'tu maldito amor', '$2y$10$tSp7J9PiKUSDGbDmXooZVeGxF7qUMDUPKmxjj4skDmTM0dciUeaoK', 'Empleado', 3, '2025-09-02 01:49:18', '2025-09-02 02:03:10', 'f2e2314ad6f1c882e9bd8950b2796dcc7215dfa25cc913646ae4c6a60cc6a5e2', '2025-09-02 05:03:10');
+INSERT INTO `users` (`id`, `nombre`, `email`, `telefono`, `direccion`, `password`, `rol`, `estado`, `area_id`, `created_at`, `updated_at`, `token`, `token_expiracion`) VALUES
+(1, 'laura', 'laura@gmail.com', 0, '', '$2y$10$ZXBSzMa1BC66TPeJow6R7OVvj8hFgOmmL4/jiVSWSCaIw2N.lylxu', 'Admin', 'activo', 1, '2025-08-12 18:12:46', '2025-09-02 01:45:06', 'fd3b9661fd59182725690bd0bfcc8c57690b7be3aa5c1369f3d1f5ad4058e71e', '2025-09-02 04:45:06'),
+(4, 'daniela', 'daniela@gmail.com', 2147483647, 'ddjhshdushdus', '$2y$10$bwDpMomBmmWgwHxeENUSseTK8v/zpo.oCOnfe5v5mCQ5FuM8M2brW', 'Empleado', 'activo', 2, '2025-08-26 18:44:00', '2025-08-31 18:02:28', NULL, NULL),
+(5, 'rodrigo', 'rodrigo@gmail.com', 2147483647, 'tu maldito amor', '$2y$10$d6ZI2HMRc0PWzVHhM94Ccemh3LFnClBF0zN68jrOfx1a6Y.mgmm8K', 'Seguridad', 'activo', 1, '2025-08-27 21:52:08', '2025-08-27 21:52:08', NULL, NULL),
+(6, 'santiago', 'walterossanti35@gmail.com', 2147483647, 'tu maldito amor', '$2y$10$tSp7J9PiKUSDGbDmXooZVeGxF7qUMDUPKmxjj4skDmTM0dciUeaoK', 'Empleado', 'activo', 3, '2025-09-02 01:49:18', '2025-09-02 02:03:10', 'f2e2314ad6f1c882e9bd8950b2796dcc7215dfa25cc913646ae4c6a60cc6a5e2', '2025-09-02 05:03:10'),
+(7, 'pepe', 'pepe@gmail.com', 2147483647, 'dasds44sa5d4sada', '$2y$10$R8Utvbrg3q30pjNKtVXmFOyO9DNMCfLWFe9d.6xRKiw6ON8X.zcVm', 'Empleado', 'incapacitado', 1, '2025-09-03 04:03:26', '2025-09-03 04:04:43', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -184,7 +193,9 @@ CREATE TABLE `visitantes` (
 --
 
 INSERT INTO `visitantes` (`id`, `nombre`, `documento`, `empresa`, `fecha_ingreso`, `fecha_salida`, `motivo`) VALUES
-(2, 'saul', '123456789', 'claro', '2025-08-23 15:20:00', '2025-08-29 15:20:00', 'dasdsadsad');
+(2, 'saul', '123456789', 'claro', '2025-08-23 15:20:00', '2025-08-29 15:20:00', 'dasdsadsad'),
+(3, 'samiel', '789645312', 'televisiones', '2025-09-11 12:30:00', '1970-01-01 03:02:00', 'arreglar televisiones'),
+(6, 'eqwewq', '789456123789', 'gfdgdfgf', '2025-09-25 14:39:00', '0000-00-00 00:00:00', 'gdfgdfgdfgd');
 
 --
 -- Índices para tablas volcadas
@@ -275,7 +286,7 @@ ALTER TABLE `documentos`
 -- AUTO_INCREMENT de la tabla `horarios`
 --
 ALTER TABLE `horarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `permisos`
@@ -287,13 +298,13 @@ ALTER TABLE `permisos`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `visitantes`
 --
 ALTER TABLE `visitantes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
